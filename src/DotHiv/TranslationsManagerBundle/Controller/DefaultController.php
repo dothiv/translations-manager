a1be83568d345bdd188e88d93e31ee0d1add9eeb
Nils Wisiol <info@nils-wisiol.de>
2013-11-06 11:32:12 +0100

    feat(parser): parsing and checking of CSV file
    Implemented basic checking of the CSV file. Checks are, among others:
    - duplicate key use
    - wrong key names

--- src/DotHiv/TranslationsManagerBundle/Controller/DefaultController.php
+++ src/DotHiv/TranslationsManagerBundle/Controller/DefaultController.php
@@ -2,12 +2,27 @@
 
 namespace DotHiv\TranslationsManagerBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('DotHivTranslationsManagerBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('DotHivTranslationsManagerBundle:Default:index.html.twig');
     }
+
+//     public function getCsvAction($key)
+//     {
+//         $url = 'https://docs.google.com/spreadsheet/ccc?key=' . $key . '&output=csv&hl&pref=2';
+//         return new Response($url);
+//         $ch = curl_init($url);
+//         $ckfile = tempnam("/tmp", "curlcookie");
+//         curl_setopt ($ch, CURLOPT_COOKIEJAR, $ckfile);
+//         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+//         $csv = curl_exec($ch);
+//         unlink($ckfile);
+//         return new Response($csv);
+//     }
 }
